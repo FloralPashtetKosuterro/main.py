788ee16daa665e4b8c7984295c7cb844186cb923
FloralPashtetKosuterro <hahatun2121@gmail.com>
2023-09-05 10:21:02 +0300

    Создаём список и замещаем чётные числа

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 chislo = int(input("Введите число больше нуля: "))
-stroka = input("Введите слово: ")
+stroka = input("Введите слово, которое заменит чётное число: ")
 spisok = list(range(1, chislo + 1))
 
 for g in range(len(spisok)):
